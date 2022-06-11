d431a3494b2df85294e89bc224d6a6aa97f38a6d
Sam Sycamore <71297412+samuelsycamore@users.noreply.github.com>
2022-06-10 22:33:31 -0500

    more cleanup

--- pages/404.js
+++ pages/404.js
@@ -10,12 +10,12 @@ export default function FourZeroFour() {
       </div>
       <div className="max-w-md">
         <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">
-          Sorry we couldn't find this page.
+          That page doesn't exist.
         </p>
-        <p className="mb-8">But dont worry, you can find plenty of other things on our homepage.</p>
+        <p className="mb-8">Sorry! Not much we can do here.</p>
         <Link href="/">
           <button className="focus:shadow-outline-blue inline rounded-lg border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-blue-700 focus:outline-none dark:hover:bg-blue-500">
-            Back to homepage
+            Let's go home
           </button>
         </Link>
       </div>
