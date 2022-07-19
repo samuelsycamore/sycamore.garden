3d1a560ef3e0f330f4e0c94712fab6e3c7787b1c
Sam Sycamore <71297412+samuelsycamore@users.noreply.github.com>
2022-07-18 22:27:35 -0500

    shop

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -2,6 +2,7 @@ const headerNavLinks = [
   { href: 'https://tech.sycamore.garden', title: 'Tech' },
   { href: 'https://foraging.sycamore.garden', title: 'Foraging' },
   { href: 'https://sycamore.garden/music', title: 'Music' },
+  { href: 'https://shop.sycamore.garden', title: 'Shop' },
   { href: 'https://work.sycamore.garden', title: 'Work' },
   { href: '/', title: 'About' },
 ]
